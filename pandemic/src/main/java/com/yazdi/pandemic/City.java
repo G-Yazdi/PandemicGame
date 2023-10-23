3351b42814377a26b5eadd4037ef6d15a0f86f79
G-Yazdi <hg.yazdi@gmail.com>
2023-10-23 14:03:03 +0330

    Implements the addNeighbour method

--- pandemic/src/main/java/com/yazdi/pandemic/City.java
+++ pandemic/src/main/java/com/yazdi/pandemic/City.java
@@ -1,10 +1,27 @@
 package com.yazdi.pandemic;
 
+import java.util.ArrayList;
+
 /**
- * Hello world!
+ *
  *
  */
 public class City 
 {
+	ArrayList<City> neighbours = new ArrayList<City>();
+	String name;
+	
+	public City(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList<City> getNeighbours() {
+		return this.neighbours;
+	}
+	
+	public void addNeighbour(City city) {
+		this.neighbours.add(city);
+	}
+	
     
 }
