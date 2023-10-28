5119d34b27331985bfcaec919d93bea6c57800d5
G-Yazdi <hg.yazdi@gmail.com>
2023-10-28 13:48:23 +0330

    Refactors FindCureAction

--- pandemic/src/main/java/com/yazdi/pandemic/FindCureAction.java
+++ pandemic/src/main/java/com/yazdi/pandemic/FindCureAction.java
@@ -1,29 +1,21 @@
 package com.yazdi.pandemic;
 
-
-import java.util.ArrayList;
-import java.util.Iterator;
+import com.yazdi.pandemic.utils.CustomArrayList;
 
 public class FindCureAction implements Action {
 	
 	private Disease disease;
-	private ArrayList<Disease> diseases;
+	private CustomArrayList<Disease> diseases;
 	
-	public FindCureAction(Disease disease, ArrayList<Disease> diseases) {
+	public FindCureAction(Disease disease, CustomArrayList<Disease> diseases) {
 		this.disease = disease;
 		this.diseases = diseases;
 	}
 
 	@Override
 	public void act() {
-		Iterator<Disease> diseaseIterator = diseases.iterator();
-		while(diseaseIterator.hasNext()) {
-			Disease d = diseaseIterator.next();
-			if(d.getName() == this.disease.getName()) {
-				d.setHasCure(true);
-				break;
-			}	
-		}
+		Disease disease = this.diseases.findIfCustom(d->d.getName() == this.disease.getName());
+		disease.setHasCure(true);
 	}
 
 }
