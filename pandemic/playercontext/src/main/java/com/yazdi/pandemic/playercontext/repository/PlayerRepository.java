64fc1e86ef54b6c0ad8f3d27a814059839f7813a
G-Yazdi <hg.yazdi@gmail.com>
2023-11-08 10:56:09 +0330

    Adds updatePlayerLocationResearchStationStatus to player repository

--- pandemic/playercontext/src/main/java/com/yazdi/pandemic/playercontext/repository/PlayerRepository.java
+++ pandemic/playercontext/src/main/java/com/yazdi/pandemic/playercontext/repository/PlayerRepository.java
@@ -4,4 +4,5 @@ import com.yazdi.pandemic.playercontext.model.City;
 
 public interface PlayerRepository {
 	void updatePlayerLocation(int playerId, City destination);
+	void updatePlayerLocationResearchStationStatus(int playerId, boolean hasResearchStation);
 }
