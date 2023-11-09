8c72ae44894aa3f1010db06d0026003eded80539
G-Yazdi <hg.yazdi@gmail.com>
2023-11-09 11:13:03 +0330

    Adds and implements findCureService

--- pandemic/api/src/main/java/com/yazdi/pandemic/api/PlayerServiceApi.java
+++ pandemic/api/src/main/java/com/yazdi/pandemic/api/PlayerServiceApi.java
@@ -1,6 +1,7 @@
 package com.yazdi.pandemic.api;
 
 import com.yazdi.pandemic.playercontext.model.City;
+import com.yazdi.pandemic.playercontext.model.Disease;
 import com.yazdi.pandemic.playercontext.model.Player;
 import com.yazdi.pandemic.playercontext.service.IPlayerService;
 
@@ -17,6 +18,11 @@ private IPlayerService playerService;
 	}
 	public void buildService(Player player) {
 		 playerService.buildResearchStation(player);
+	}
+
+	public void findCureService(Player player, Disease disease) {
+		playerService.findCure(player, disease);
+		
 	}
 
 }
