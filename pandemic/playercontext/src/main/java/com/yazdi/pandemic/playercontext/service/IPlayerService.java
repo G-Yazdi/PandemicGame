a230edc11a5eda8b07316ff89469e2a3671d6c68
G-Yazdi <hg.yazdi@gmail.com>
2023-11-09 10:47:51 +0330

    Adds findCure service to the IPlayerService.

--- pandemic/playercontext/src/main/java/com/yazdi/pandemic/playercontext/service/IPlayerService.java
+++ pandemic/playercontext/src/main/java/com/yazdi/pandemic/playercontext/service/IPlayerService.java
@@ -1,14 +1,17 @@
 package com.yazdi.pandemic.playercontext.service;
 
 import com.yazdi.pandemic.playercontext.model.City;
+import com.yazdi.pandemic.playercontext.model.Disease;
 import com.yazdi.pandemic.playercontext.model.Player;
 import com.yazdi.pandemic.playercontext.repository.PlayerRepository;
 import com.yazdi.pandemic.sharedkernel.service.ApplicationService;
 
 public interface IPlayerService extends ApplicationService {
 	
-    void movePlayer(Player player, City destination);
-    void buildResearchStation(Player player);
+    public void movePlayer(Player player, City destination);
+    public void buildResearchStation(Player player);
+    public void findCure(Player player, Disease disease);
+    
 
-	void setPlayerRepository(PlayerRepository playerRepository);
+	public void setPlayerRepository(PlayerRepository playerRepository);
 }
