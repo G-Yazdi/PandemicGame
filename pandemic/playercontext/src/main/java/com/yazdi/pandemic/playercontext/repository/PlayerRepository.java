7db83b554f97936ee5d52420cc158a6041a6bef7
G-Yazdi <hg.yazdi@gmail.com>
2023-11-09 11:11:49 +0330

    Adds updatePlayerHand and updatePlayerLocationDiseaseStatus

--- pandemic/playercontext/src/main/java/com/yazdi/pandemic/playercontext/repository/PlayerRepository.java
+++ pandemic/playercontext/src/main/java/com/yazdi/pandemic/playercontext/repository/PlayerRepository.java
@@ -1,8 +1,13 @@
 package com.yazdi.pandemic.playercontext.repository;
 
+import com.yazdi.pandemic.playercontext.model.Card;
 import com.yazdi.pandemic.playercontext.model.City;
+import com.yazdi.pandemic.playercontext.model.CustomArrayList;
+import com.yazdi.pandemic.playercontext.model.Disease;
 
 public interface PlayerRepository {
 	void updatePlayerLocation(int playerId, City destination);
 	void updatePlayerLocationResearchStationStatus(int playerId, boolean hasResearchStation);
+	void updatePlayerHand(int playerId, CustomArrayList<Card> playerHand);
+	void updatePlayerLocationDiseaseStatus(int playerId, Disease disease, boolean hasCure);
 }
