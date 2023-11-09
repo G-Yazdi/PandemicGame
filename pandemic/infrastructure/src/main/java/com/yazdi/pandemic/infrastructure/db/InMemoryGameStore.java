1904d2d24e8d901733a2d6e59bf91578b6aa6a63
G-Yazdi <hg.yazdi@gmail.com>
2023-11-09 11:54:06 +0330

    Refactors InMemoryGameStore and implements two methods of it

--- pandemic/infrastructure/src/main/java/com/yazdi/pandemic/infrastructure/db/InMemoryGameStore.java
+++ pandemic/infrastructure/src/main/java/com/yazdi/pandemic/infrastructure/db/InMemoryGameStore.java
@@ -3,16 +3,23 @@ package com.yazdi.pandemic.infrastructure.db;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.yazdi.pandemic.playercontext.model.Card;
 import com.yazdi.pandemic.playercontext.model.City;
+import com.yazdi.pandemic.playercontext.model.CustomArrayList;
+import com.yazdi.pandemic.playercontext.model.Disease;
 import com.yazdi.pandemic.playercontext.repository.PlayerRepository;
 
 public class InMemoryGameStore implements PlayerRepository {
-    private Map<Integer, City> gameDb = new HashMap<>();
+    private Map<Integer, PersistencePlayer> gameDb = new HashMap<>();
     private volatile static InMemoryGameStore instance = new InMemoryGameStore();
 
     @Override
-    public void updatePlayerLocation(int playerId, City destination){
-        this.gameDb.put(playerId, destination);
+    public void updatePlayerLocation(int playerId, City newCity){
+    	PersistencePlayer player = this.gameDb.get(playerId);
+    	if(player != null) {
+    		player.playerLocation = newCity;
+    		this.gameDb.put(playerId, player);
+    	}
     }
 
     public static InMemoryGameStore provider() {
@@ -21,17 +28,42 @@ public class InMemoryGameStore implements PlayerRepository {
 
 	@Override
 	public void updatePlayerLocationResearchStationStatus(int playerId, boolean hasResearchStation) {
-		City playerLocation;
-		if(this.gameDb.get(playerId) != null) {
-			playerLocation = this.gameDb.get(playerId);
-			playerLocation.setHasResearchStation(hasResearchStation);
-		}
-		else {
-			playerLocation = new City("defaultCity");
-			playerLocation.setHasResearchStation(true);
-		}
-		this.gameDb.put(playerId, playerLocation);
-		
+		PersistencePlayer player = this.gameDb.get(playerId);
+    	if(player != null) {
+    		player.playerLocation.setHasResearchStation(hasResearchStation);
+    		this.gameDb.put(playerId, player);
+    	}
+	}
+
+	@Override
+	public void updatePlayerHand(int playerId, CustomArrayList<Card> playerHand) {
+		PersistencePlayer player = this.gameDb.get(playerId);
+    	if(player != null) {
+    		player.playerHand = playerHand;
+    		this.gameDb.put(playerId, player);
+    	}
 		
 	}
+
+	@Override
+	public void updatePlayerLocationDiseaseStatus(int playerId, Disease disease, boolean hasCure) {
+		PersistencePlayer player = this.gameDb.get(playerId);
+    	if(player != null) {
+    		player.playerLocation.getCubesOf(disease).forEach(d -> d.setHasCure(hasCure));
+    		this.gameDb.put(playerId, player);
+    	}
+		
+	}
+	
+	public static class PersistencePlayer {
+		public int playerId;
+	    public City playerLocation;
+	    public CustomArrayList<Card> playerHand;
+
+	    public PersistencePlayer(int playerId, City playerLocation, CustomArrayList<Card> playerHand) {
+	    	this.playerId = playerId;
+	        this.playerLocation = playerLocation;
+	        this.playerHand = playerHand;
+	    }
+	 }
 }
