c1a3db3a93cdf167bbb82f9fc2d4cc48248523e2
G-Yazdi <hg.yazdi@gmail.com>
2023-11-13 13:23:29 +0330

    Updates infrastructure's dependencies

--- pandemic/infrastructure/src/main/java/module-info.java
+++ pandemic/infrastructure/src/main/java/module-info.java
@@ -4,8 +4,11 @@ import com.yazdi.pandemic.infrastructure.events.SimpleEventBus;
 module com.yazdi.pandemic.infrastructure {
     requires transitive com.yazdi.pandemic.sharedkernel;
     requires transitive com.yazdi.pandemic.playercontext;
+    requires transitive com.yazdi.pandemic.mapcontext;
     provides com.yazdi.pandemic.sharedkernel.events.EventBus
       with SimpleEventBus;
     provides com.yazdi.pandemic.playercontext.repository.PlayerRepository
       with InMemoryGameStore;
+    provides com.yazdi.pandemic.mapcontext.repository.MapRepository
+    with InMemoryGameStore;
 }
