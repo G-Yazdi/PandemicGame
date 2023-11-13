a4393e7728c22500a438b801192e8a99593b7082
G-Yazdi <hg.yazdi@gmail.com>
2023-11-13 12:16:59 +0330

    Implements WorldMap

--- pandemic/mapcontext/src/main/java/com/yazdi/pandemic/mapcontext/model/entities/WorldMap.java
+++ pandemic/mapcontext/src/main/java/com/yazdi/pandemic/mapcontext/model/entities/WorldMap.java
@@ -1,5 +1,22 @@
 package com.yazdi.pandemic.mapcontext.model.entities;
 
+
 public class WorldMap {
+	
+	CustomArrayList<ICity> cities;
+	
+	public WorldMap() {
+		cities = new CustomArrayList<ICity>();
+	}
+	public ICity getCity(int cityId) {
+		return cities.findIfCustom(c -> ((City)c).getId() == cityId);
+		
+	}
+	public CustomArrayList<ICity> getCities() {
+		return this.cities;
+	}
+	public void addCity(ICity newCity) {
+		this.cities.add(newCity);
+	}
 
 }
