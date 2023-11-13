dc5cd80f7a2092523f84c36bd45b34bcc5bfb5e8
G-Yazdi <hg.yazdi@gmail.com>
2023-11-13 12:50:35 +0330

    Updates IMapService dependencies

--- pandemic/mapcontext/src/main/java/com/yazdi/pandemic/mapcontext/service/IMapService.java
+++ pandemic/mapcontext/src/main/java/com/yazdi/pandemic/mapcontext/service/IMapService.java
@@ -1,6 +1,7 @@
 package com.yazdi.pandemic.mapcontext.service;
 
 import com.yazdi.pandemic.mapcontext.model.entities.City;
+import com.yazdi.pandemic.mapcontext.model.entities.WorldMap;
 import com.yazdi.pandemic.mapcontext.repository.MapRepository;
 import com.yazdi.pandemic.sharedkernel.service.ApplicationService;
 
