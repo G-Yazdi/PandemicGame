9e1a4928ef07aa51eb5ebc86c262d1131407a308
G-Yazdi <hg.yazdi@gmail.com>
2023-11-13 12:29:56 +0330

    Exports utilt from sharedKernel

--- pandemic/sharedkernel/src/main/java/module-info.java
+++ pandemic/sharedkernel/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.yazdi.pandemic.sharedkernel {
     exports com.yazdi.pandemic.sharedkernel.events;
     exports com.yazdi.pandemic.sharedkernel.service;
+    exports com.yazdi.pandemic.sharedkernel.utils;
 }
