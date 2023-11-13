a68320f120ce1ca5e843aeb7ff4421e8bf7e6e80
G-Yazdi <hg.yazdi@gmail.com>
2023-11-13 12:08:40 +0330

    Updates com.yazdi.pandemic.api module

--- pandemic/api/src/main/java/module-info.java
+++ pandemic/api/src/main/java/module-info.java
@@ -2,7 +2,10 @@ module com.yazdi.pandemic.api {
     uses com.yazdi.pandemic.sharedkernel.events.EventBus;
     uses com.yazdi.pandemic.playercontext.service.IPlayerService;
     uses com.yazdi.pandemic.playercontext.repository.PlayerRepository;
+    uses com.yazdi.pandemic.mapcontext.service.IMapService;
+    uses com.yazdi.pandemic.mapcontext.repository.MapRepository;
     requires transitive com.yazdi.pandemic.infrastructure;
 	requires transitive com.yazdi.pandemic.playercontext;
 	requires transitive com.yazdi.pandemic.sharedkernel;
+	requires transitive com.yazdi.pandemic.mapcontext;
 }
